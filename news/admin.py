8f23c07fc262bab2728b1243bb189c3d756fc2f9
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2021-05-01 23:28:12 +0500

    push database to github

--- news/admin.py
+++ news/admin.py
@@ -1,28 +1,35 @@
 from django.contrib import admin
 
-from .models import (Category,Region,Article,Attachment,Message,Employee)
+from .models import (Category, Region, Article, Attachment, Message, Employee)
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name','slug','description')
+    list_display = ('name', 'slug', 'description')
+
 
 @admin.register(Region)
 class RegionAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'description')
 
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title','category','published_at','review','slug')
+    list_display = ('title', 'category', 'published_at', 'review', 'slug')
     search_fields = ['title']
+
 
 @admin.register(Attachment)
 class AttachmentAdmin(admin.ModelAdmin):
-    list_display = ('article','order','image')
+    list_display = ('article', 'order', 'image')
     autocomplete_fields = ['article']
+
+
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('full_name','email','phone_number','theme','created_at')
+    list_display = ('full_name', 'email', 'phone_number', 'theme', 'created_at')
+
 
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('full_name','position')
+    list_display = ('full_name', 'position')
--- news/views.py
+++ news/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from news.models import Category, Region
+
+
 def home(request):
-    return HttpResponse("<h3>Hello World !</h3>")
+    categories = Category.objects.all().order_by('pk')
+    regions = Region.objects.all().order_by('pk')
+    data = {
+        'categories': categories,
+        'regions': regions,
+    }
+    return render(request, 'news/base.html', context=data)
