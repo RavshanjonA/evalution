b72def038dec23d73d56bafb69c643019b87b1d1
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2021-05-30 22:28:17 +0500

    add contact form url

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from news.views import home, category_item, region_item
+from news.views import home, category_item, region_item, contactform
 
 app_name = 'news'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('news/category/<slug>', category_item, name='category-item'),
     path('region/<slug>', region_item, name='region-item'),
+    path('contact/', contactform, name='contact'),
 
 ]
